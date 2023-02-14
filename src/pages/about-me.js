7cc68b661c65e32d18e0b92f24a9ab69e0eb372d
cesarcuadro <cesarcuadro777@gmail.com>
2023-02-14 10:59:52 -0800

    added structure to about me page

--- src/pages/about-me.js
+++ src/pages/about-me.js
@@ -1,4 +1,6 @@
 import Link from "next/link";
+import Image from "next/image";
+import project from 'public/project.png'
 
 export default function AboutMe() {
   return (
@@ -7,6 +9,17 @@ export default function AboutMe() {
       <h2>
         <Link href="/">Back to home</Link>
       </h2>
+      <section>
+        <h3>Tech Skills</h3>
+        <p>skill 1</p>
+      </section>
+      <section>
+        <Image
+        src={project}
+        alt="placeholder for professional picture"
+        />
+        <p>Ea anim Lorem veniam velit anim Lorem nisi id incididunt. Pariatur fugiat eu occaecat dolor occaecat. Aliquip eu pariatur adipisicing reprehenderit labore reprehenderit cupidatat culpa minim mollit laboris duis. Minim ipsum consequat quis id magna veniam ea.</p>
+      </section>
     </>
   );
 }
