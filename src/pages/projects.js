ea295bc63790f2033def49c7ff87f908fd5ecd89
cesarcuadro <cesarcuadro777@gmail.com>
2023-02-16 23:43:06 -0800

    changed default state for project.js

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -3,46 +3,47 @@ import Image from "next/image";
 import projectData from "../data/projectData.js";
 
 const Projects = () => {
-  const [project, setProject] = useState("");
+  const [project, setProject] = useState(projectData[0]);
   const [menu, setMenu] = useState("collapse");
   const handleClick = (id) => {
-    setProject(projectData[id-1]);
-    handleMenu()
+    setProject(projectData[id - 1]);
+    handleMenu();
   };
 
   const handleMenu = () => {
     if (menu === "collapse") {
-      setMenu("visible")
+      setMenu("visible");
     } else if (menu === "visible") {
-      setMenu("collapse")
+      setMenu("collapse");
     }
-    console.log(menu)
-  } 
-console.log(project)
+    console.log(menu);
+  };
+  console.log(project);
   return (
     <section className="m-2 font-serif ">
       <h1 className="text-xl">My projects</h1>
-      <button onClick={handleMenu} className="border-2 border-black rounded-xl text-yellow-300 bg-purple-900">Choose:</button>
+      <button
+        onClick={handleMenu}
+        className="rounded-xl border-2 border-black bg-purple-900 text-yellow-300"
+      >
+        Choose:
+      </button>
 
       <div className={`flex flex-col ${menu}`}>
-      {projectData?.map((project, index) => {
-        return <button key={index}  onClick={() => handleClick(project.id)}>
-          <div className="">
-          {project.title}
-          </div>
-          </button>;
-          
-      })}
+        {projectData?.map((project, index) => {
+          return (
+            <button key={index} onClick={() => handleClick(project.id)}>
+              <div className="">{project.title}</div>
+            </button>
+          );
+        })}
       </div>
 
-      <div>
-      {project?.title}
-      </div>
+      <div>{project?.title}</div>
       <div>{project?.description}</div>
       <div>
-      {project?.image && <Image src={project.image} alt="project preview" />}
+        {project?.image && <Image src={project.image} alt="project preview" />}
       </div>
-
     </section>
   );
 };
