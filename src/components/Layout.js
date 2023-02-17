6aaf0722098a9e1dbfc5efa2cf0c73734851dee6
cesarcuadro <cesarcuadro777@gmail.com>
2023-02-17 12:30:19 -0800

    set bg dark with white text

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -3,7 +3,7 @@ import Header from "./Header";
 export default function Layout({ children }) {
   return (
     <>
-      <div className="font-serif bg-slate-600 h-[1000px]">
+      <div className="font-serif text-white bg-slate-700 min-h-screen overflow-y-auto">
         <Header />
         <main>{children}</main>
       </div>
