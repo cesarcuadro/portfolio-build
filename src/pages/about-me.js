2d7a513d6e1be08cb56de19d570647ca1b0097dd
cesarcuadro <cesarcuadro777@gmail.com>
2023-02-16 23:34:00 -0800

    changed spacing in about page

--- src/pages/about-me.js
+++ src/pages/about-me.js
@@ -13,12 +13,12 @@ import { faGem } from "@fortawesome/free-solid-svg-icons";
 
 export default function AboutMe() {
   return (
-    <div>
+    <div className="space-y-4">
       <div className="flex justify-center">
         <Image src={project} alt="" className="rounded-full w-16 h-16 m-2 border-2 border-black"/>
       </div>
       
-      <section className="border-2 border-black m-2 rounded-xl p-2">
+      <section className="border-2 border-black m-2 rounded-xl p-2 space-y-1">
         <h3 className="text-xl text-center">Technical Skills</h3>
         <div className="flex justify-evenly">
           <FontAwesomeIcon icon={faReact} />
@@ -30,6 +30,7 @@ export default function AboutMe() {
           <FontAwesomeIcon icon={faBootstrap} />
         </div>
       </section>
+
       <section className="border-2 border-black m-2 rounded-xl p-2 object-center">
       <h1 className="font-bold text-m">My story:</h1>
        <p> Full stack web developer with strong analytical and communication skills. Dedicated to discovering, developing, and implementing creative solutions to complex web application challenges. Extensive experience in troubleshooting steps from current role as a ground radio maintainer in the Marine Corps Reserves.</p>
